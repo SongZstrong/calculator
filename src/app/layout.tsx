2929fe17d4a75751bc71254238e1b6b89fcbd57f
sw <sz41@swdeMacBook-Air.local>
2025-07-29 22:57:24 +0800

    chore: add Google AdSense script to <head> of all pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8115477477403051" crossOrigin="anonymous"></script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
